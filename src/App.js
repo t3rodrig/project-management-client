226c656d3706eba88aee23c6ba6169c0cbc4de8b
Tonalli <tonalli@xanum.uam.mx>
2020-03-06 17:11:57 -0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 import ProjectList from './components/projects/ProjectList';
+import Navbar from './components/navbar/Navbar';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 class App extends Component {
   render() {  
     return (
       <div className="App">
-        <ProjectList />
+       <Navbar />
+        <Switch>
+          <Route exact path="/projects" component={ProjectList}/>
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+        </Switch>
       </div>
     );
   }
