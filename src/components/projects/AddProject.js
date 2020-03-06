e166b602b859f8872be2dcc6a0d247f863259e1f
Tonalli <tonalli@xanum.uam.mx>
2020-03-06 14:37:28 -0600

    Add AddProject component

---
+++ src/components/projects/AddProject.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AddProject extends Component {
+  constructor(props){
+      super(props);
+      this.state = { title: "", description: ""};
+    }
+
+  handleFormSubmit = (event) => {
+    event.preventDefaul();
+    const { title, description } = this.state;
+    axios.post("http://localhost:5000/api/projects", { title, description })
+      .then( () => {
+        // this.props.getData();
+        this.setState({title: "", description: ""});
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({[name]: value});
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>Tite:</label>
+          <input 
+            type="text" 
+            name="title" 
+            value={this.state.title}
+            onChange={event => this.handleChange(event)}
+          />
+          <label>Description:</label>
+          <textarea 
+            name="description" 
+            value={this.state.description} 
+            onChange={event => this.handleChange(event)}
+          />
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
