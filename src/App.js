b0f34e490de1b634ddd40a3898e6d0b5132b8e10
Tonalli <tonalli@xanum.uam.mx>
2020-03-06 16:21:15 -0600

    Update app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import AddProject from './components/projects/AddProject';
+import ProjectList from './components/projects/ProjectList';
 
 class App extends Component {
   render() {  
     return (
       <div className="App">
-        <AddProject />
+        <ProjectList />
       </div>
     );
   }
