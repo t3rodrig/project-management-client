0c1a452ea8adaf901d68b29aae681f35725a628d
Tonalli <tonalli@xanum.uam.mx>
2020-03-21 11:05:23 -0600

    Update ProjectList.js
    Add tasks to ProjectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -33,6 +33,12 @@ class ProjectList extends Component {
                 <Link to={`/projects/${project._id}`}>
                   <h3>{project.title}</h3>
                 </Link>
+                {/* added so the tasks can be desplayed: */}
+                <ul>
+                  {project.tasks.map((task, index) => {
+                    return <li key={index}>{task.title}</li>
+                  })}
+                </ul>
                 {/* <p style={{maxWidth: '400px'}}>{project.description}</p> */}
               </div>
             )
