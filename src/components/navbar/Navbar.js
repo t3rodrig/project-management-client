78fb788f04bb0cb73f3f9620c048883f49bf5a26
Tonalli <tonalli@xanum.uam.mx>
2020-03-23 21:23:52 -0600

    Update Navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AuthService from '../auth/Login';
 
 class navbar extends Component {
   constructor(props){
     super(props)
     this.state = { loggedInUser: null };
+    this.service = new AuthService();
   }
 
   componentWillReceiveProps(nextProps){
     this.setState({...this.state, loggedInUser: nextProps['userInSession']});
+  }
+
+  logoutUser = () => {
+    this.service.logout()
+    .then(() => {
+      this.setState({ loggedInUser: null });
+      this.props.getUser(null);
+    });
   }
 
   render() {
@@ -20,13 +30,19 @@ class navbar extends Component {
             <li>
               <Link to="/projects" style={{textDecoration: 'none'}}>Projects</Link>
             </li>
+            <li>
+              <Link to="/"><button onClick={() => this.logoutUser()}>Logout</button></Link>
+            </li>
           </ul>
         </nav>
       );
     } else {
       return (
-        <nav>
+        <nav className="nav-style">
           <ul>
+            <li>
+              <Link to="/" style={{textDecoration: 'none'}}>Login</Link>
+            </li>
             <li>
               <Link to="/signup" style={{textDecoration: 'none'}}>Signup</Link>
             </li>
