18f0a594a75d0c3d3f0b23c660d50694e3985356
Tonalli <tonalli@xanum.uam.mx>
2020-03-23 22:08:16 -0600

    Fix AuthService import

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import AuthService from '../auth/Login';
+import AuthService from '../auth/auth-service';
 
 class navbar extends Component {
   constructor(props){
